8e84bbf5978e36f4582f0caf00b84bb7fb93d8fa
Reid <thraveboy@yahoo.com>
2014-09-07 02:11:32 -0700

    Wider standard map and standard worlds & displays

--- open_hud.sh
+++ open_hud.sh
@@ -1,3 +1,3 @@
-xterm -bg black -fg green -cr purple +cm +dc -geometry 20x12+900+0 -title "Map" -e tail -f display_logs/map_log &
-xterm -bg black -fg green -cr purple +cm +dc -geometry 80x30+1040+0 -title "Team" -e tail -f display_logs/team_log  &
-xterm -bg black -fg green -cr purple +cm +dc -geometry 100x30+900+425 -title "Battle Report" -e tail -f display_logs/battle_log  &
+xterm -bg black -fg green -cr purple +cm +dc -geometry 40x21+900+0 -title "Map" -e tail -f display_logs/map_log &
+xterm -bg black -fg green -cr purple +cm +dc -geometry 40x30+1145+0 -title "Team" -e tail -f display_logs/team_log  &
+xterm -bg black -fg green -cr purple +cm +dc -geometry 80x30+900+425 -title "Battle Report" -e tail -f display_logs/battle_log  &
