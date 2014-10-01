eb6c86606b263465c285dff4c8f99f53b8d941fd
Reid <thraveboy@yahoo.com>
2014-09-30 23:52:25 -0700

    Sweet hand-crafted font and hud invis cursor

--- open_hud.sh
+++ open_hud.sh
@@ -1,3 +1,3 @@
-xterm -bg black -fg green -cr white -ms black -fa impact -fs 10 -uc +cm +dc -geometry 21x12+800+0 -title "Map" -e tail -f display_logs/map_log &
+xterm -bg black -ah -fg green -cr white -ms black -fa reidsfont -fs 10 -uc +cm +dc -geometry 21x12+750+0 -title "Map" -e "tput civis; tail -f display_logs/map_log" &
 xterm -bg black -fg green -cr purple +cm +dc -geometry 60x45+1080+0 -title "Team" -e tail -f display_logs/team_log  &
-xterm -bg black -fg green -cr purple +cm +dc -geometry 110x10+800+620 -title "Battle Report" -e tail -f display_logs/battle_log  &
+xterm -bg black -fg green -cr purple +cm +dc -geometry 110x10+750+620 -title "Battle Report" -e tail -f display_logs/battle_log  &
